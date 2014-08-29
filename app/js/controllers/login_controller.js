776463ef5135b6f199b2c320accdf14c115620ad
Joao Guerreiro <jmdguerreiro@gmail.com>
2014-08-29 23:19:16 +0200

    removed password from credentials to login

--- app/js/controllers/login_controller.js
+++ app/js/controllers/login_controller.js
@@ -1,5 +1,5 @@
 angular.module("app").controller('LoginController', function($scope, $location, AuthenticationService) {
-  $scope.credentials = { username: "", password: "" };
+  $scope.credentials = { username: ""};
 
   var onLoginSuccess = function() {
     $location.path('/home');
