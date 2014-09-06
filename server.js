5735c8070c4bd4e072de3c8047eedba600db0f18
Joao Guerreiro <jmdguerreiro@gmail.com>
2014-09-07 01:06:25 +0200

    removed mocha testing of socket io broadcast. add html for helping to test server

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-var io = require('socket.io').listen(5000);
-var clients = {};
+var io = require('socket.io').listen(5001);
+
 
 io.sockets.on('connection', function (socket) {
   console.log("client connection");
   socket.on('user:connect',function(userName){
-    console.log("user connection request from: " + userName);
-    socket.emit('user:connected', userName);
+    console.log("user:connect request from: " + userName);
+    io.sockets.emit('user:connected', userName);
+  });
+
+  socket.on('disconnect',function(){
+    console.log("user disconnected");
   });
   
 });
